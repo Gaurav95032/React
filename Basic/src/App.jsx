4e1b1e8b7fecd7d32ac7723cf7093f439237efbe
[Gaurav Waghmare] <gauravwaghmare95032@gmail.com>
2025-06-07 22:43:47 +0530

    Rename counter function to counterAdd and add counterRemove for better clarity

--- Basic/src/App.jsx
+++ Basic/src/App.jsx
@@ -3,14 +3,17 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  const counter = () => {
+  const counterAdd = () => {
     setCount(prevCounter => prevCounter + 1);
   }
-
+  const counterRemove = () => {
+    setCount(prevCounter => prevCounter - 1);
+  }
   return (
     <>
       <h1>Count : {count}</h1>
-      <button onClick={counter} >Update Counter</button>
+      <button  onClick={counterAdd} >Update Counter</button>
+      <button onClick={counterRemove} >Remove Counter</button>
     </>
   )
 }
